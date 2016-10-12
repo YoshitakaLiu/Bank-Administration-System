919a56d4d1f403c4e74ee1c3357a2f84de8c8e5d
YoshitakaLiu <377623086@qq.com>
2016-10-12 18:59:09 +0800

    Porject finished

--- src/pro1/Customer_and_Staff/User_Money.java
+++ src/pro1/Customer_and_Staff/User_Money.java
@@ -8,6 +8,33 @@ package pro1.Customer_and_Staff;
  *
  */
 public class User_Money {
-	// to be implemented
+	public double rate;
+	int year;
+	String classification;
+	double money;
+	public User_Money(int y, double m){
+		set_rate(y);
+		money = m;
+		year = y;
+	}
+	void set_rate(int y){
+		switch(y)
+		{
+		case 0: rate = 0.01; classification = "活期存款" ;break;
+		case 1: rate = 0.027 ; classification = "一年定期"; break;
+		case 2: rate = 0.031 ; classification = "两年定期"; break;
+		case 3: rate = 0.041 ; classification = "三年定期"; break;
+		case 5: rate = 0.05 ; classification = "五年定期"; break;
+		}
+	}
+	public void show(){
+		System.out.println("金额："+money+"    利率："+rate+"    存款类别："+classification+"    存款年限："+year+"年");
+	}
+	public double get_money(){
+		return money;
+	}
+	public void set_money(double x){//主要用来设置活期存款，活期存款可以随时存取；定期存款存一次就固定下来其各项信息
+		money += x;
+	}
 
 }
--- src/pro2/Database/User_database.java
+++ src/pro2/Database/User_database.java
@@ -21,4 +21,32 @@ public class User_database {
 			cs = true;//为用户
 		return cs;
 	}
+	//通过身份证号查询用户序号
+	public int get_order(String id,User_database database){
+		int j=-1;
+			for(int i=0;i<database.customer.size();i++){
+				if(database.customer.get(i).ID.equals(id))
+					j = i;
+			}
+		return j;
+	}
+	
+	//通过用户名查询用户序号，重载函数
+	public int get_order(User_database database,String u_name){
+		int j=-1;
+		if(get_cs(u_name)){//如果是用户
+			for(int i=0;i<database.customer.size();i++){
+				if(database.customer.get(i).user_name.equals(u_name))
+					j = i;
+			}
+		}
+		else{
+			for(int i=0;i<database.staff.size();i++){
+				if(database.staff.get(i).work_ID.equals(u_name))
+					j = i;
+			}
+		}
+		return j;
+	}
+
 }
